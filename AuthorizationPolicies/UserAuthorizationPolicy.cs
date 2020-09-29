4d36c9a34a89ae217cc91c9216cb9d218a3c30af
KamalDeep <31344909+kmldpsng@users.noreply.github.com>
2020-09-29 23:13:45 +0530

    Adding required changes.

---
+++ AuthorizationPolicies/UserAuthorizationPolicy.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace AuthenticationApi.AuthorizationPolicies
+{
+    public class UserAuthorizationRequirement : IAuthorizationRequirement
+    {
+        public bool DevelopmentMode { get; set; }
+
+        public UserAuthorizationRequirement(bool developmentMode)
+        {
+            DevelopmentMode = developmentMode;
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,23 +1,23 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using AuthenticationApi.AuthorizationPolicies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Server.IISIntegration;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
 
 namespace AuthenticationApi
 {
-    public class Startup
+    public class Startup 
     {
-        public Startup(IConfiguration configuration)
+        private readonly IWebHostEnvironment _env;
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            _env = env;
         }
 
         public IConfiguration Configuration { get; }
@@ -26,6 +26,44 @@ namespace AuthenticationApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Authentication API",
+                    Description = "A simple demonstration for ASP.NET Core Web API",
+                    TermsOfService = new Uri("https://TOS.com/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Kamal Deep Singh",
+                        Email = string.Empty,
+                        Url = new Uri("https://twitter.com/_kmldp"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under LICX",
+                        Url = new Uri("https://no-licenceASofNow.com/license"),
+                    }
+                });
+            });
+            SetUpAuthorization(services);
+        }
+
+        private static void SetUpAuthorization(IServiceCollection services)
+        {
+            services.AddAuthentication(IISDefaults.AuthenticationScheme);
+            services.AddAuthorization(AddUsersAuthorization);
+            
+        }
+
+        private static void AddUsersAuthorization(AuthorizationOptions authorizationOptions)
+        {
+            //!_env.IsProduction()
+            var userAuthorizationRequirement =
+                new UserAuthorizationRequirement(true);
+            authorizationOptions.AddPolicy("Add user policy",
+                policy => { policy.Requirements.Add(userAuthorizationRequirement); });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,7 +78,17 @@ namespace AuthenticationApi
 
             app.UseRouting();
 
+            app.UseSwagger();
+
+         
             app.UseAuthorization();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Authentication API");
+                //c.RoutePrefix = string.Empty;
+            });
+
 
             app.UseEndpoints(endpoints =>
             {
